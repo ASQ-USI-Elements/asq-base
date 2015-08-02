99e6e54b8f1c4b977d735776e7d2461afd171d6d
Vasileios Triglianos <triglian@gmail.com>
2015-08-02 23:59:50 +0200

    port to polymer v1.0

--- asq-base.js
+++ asq-base.js
@@ -1,138 +1,107 @@
-var ASQ = window.ASQ = {
+var ASQ = window.ASQ || {};
 
-  // From Polymer/core-focusable
-  // @see https://github.com/Polymer/core-focusable.git
-  mixin2 : function(prototype, mixin) {
-
-    // adds a single mixin to prototype
-
-    if (mixin.mixinPublish) {
-      prototype.publish = prototype.publish || {};
-      Polymer.mixin(prototype.publish, mixin.mixinPublish);
-    }
-
-    if (mixin.mixinDelegates) {
-      prototype.eventDelegates = prototype.eventDelegates || {};
-      for (var e in mixin.mixinDelegates) {
-        if (!prototype.eventDelegates[e]) {
-          prototype.eventDelegates[e] = mixin.mixinDelegates[e];
-        }
-      }
-    }
-
-    if (mixin.mixinObserve) {
-      prototype.observe = prototype.observe || {};
-      for (var o in mixin.mixinObserve) {
-        if (!prototype.observe[o] && !prototype[o + 'Changed']) {
-          prototype.observe[o] = mixin.mixinObserve[o];
-        }
-      }
-    }
-
-    Polymer.mixin(prototype, mixin);
-
-    delete prototype.mixinPublish;
-    delete prototype.mixinDelegates;
-    delete prototype.mixinObserve;
-
-    return prototype;
-  },
-
-  /**
-   *
-   * This mixin object is for every ASQ element type, including question types and others.
-   *
-   **/
-  ElementTypeMixin : {
-
+/**
+ *
+ * This mixin object is for every ASQ element type, including question types and others.
+ *
+ */
+var elementTypeBehavior =  ASQ.elementTypeBehavior = {
+  properties : {
     // True for every ASQ element.
-    isASQElement : true,
-
-    // Denotes whether an ASQ element is a `question type`. 
-    isASQQuestionTypeElement : false,
-
-    mixinPublish: {
-      // 
-      uid: {value: "", reflect: true},
-    },
-  },
-
-  /**
-   *
-   * This mixin object is `ONLY` for question types.
-   *
-   **/
-  QuestionTypeMixin : {
-    isASQQuestionTypeElement : true,
-  },
-
-
-  RoleMixin : {
-    // use a simple enum object
-    roles: Object.freeze({
-      VIEWER: "viewer",
-      PRESENTER: "presenter",
-      TA: "ta"
-    }),
-
-    mixinPublish: {
-      // default role is 'viewer'
-      role: {value: "viewer", reflect: true}
+    isASQElement :{
+      type: Boolean,
+      value: true,
+      notify: true
     },
 
-    isValidRole: function(role) {
-      var roles = ASQ.RoleMixin.roles;
-      for (var r in roles) {
-        if (roles.hasOwnProperty(r)) {
-          if ( role == roles[r] ) {
-            return true;
-          }
-        }
-      }
-      return false;
+     // Denotes whether an ASQ element is a `question type`. 
+    isASQQuestionTypeElement : {
+      type: Boolean,
+      value: false,
+      notify: true
     },
 
-    setANF: function() {
-
-    },
-
-    /**
-     * 
-     * 1. Validate the updation of role. If the new value
-     * is not a valid one, then roll back to the old value.
-     * 
-     * 2. If the role of `outside` element is 
-     * changed, then `inside` elements' role
-     * are also changed.
-     *
-     **/
-    roleChanged: function(old, newRole) {
-      if ( this.isValidRole(newRole) ) {
-        if ( old != newRole ) {
-          this.childNodes.array().filter(function(el) {
-            return el.isASQElement;
-          }).forEach(function(x) {
-            x.role = newRole;
-          });
-
-          // redo the $
-        }
-      } else {
-        this.role = old;
-      }
-    }
-  },
-
-
-  // sugar function for mixin.
-  asqify : function(p, isQuestionType){
-    this.mixin2(p, this.RoleMixin);
-    this.mixin2(p, this.ElementTypeMixin);
-
-    if(isQuestionType){
-      p.isASQQuestionTypeElement = true;
+    uid:{
+      type: String,
+      value: "",
+      notify: true,
+      reflectToAttribute: true
     }
   }
-
 };
 
+/**
+ * This mixin object is `ONLY` for question types.
+ *
+ */
+var questionTypeBehavior = ASQ.questionTypeBehavior = {
+  properties : {
+    isASQQuestionTypeElement : {
+      type: Boolean,
+      value: true,
+      notify: true
+    } 
+  }
+};
+
+
+var roleBehavior = ASQ.roleBehavior = {
+  // use a simple enum object
+  roles: Object.freeze({
+    VIEWER: "viewer",
+    PRESENTER: "presenter",
+    TA: "ta"
+  }),
+
+  properties: {
+    role: {
+      type: String,
+      value: "viewer",
+      notify: true,
+      observer: "_roleChanged",
+      reflectToAttribute: true
+    }
+  },
+
+  _isValidRole: function(role) {
+    var roles = this.roles;
+    for (var r in roles) {
+      if (roles.hasOwnProperty(r)) {
+        if ( role == roles[r] ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
+
+
+  /**
+   * 
+   * 1. Validate role update. If the new value
+   * is not a valid one, then roll back to the old value.
+   * 
+   * 2. If the role of `outside` element is 
+   * changed, then `inside` elements' role
+   * are also changed.
+   *
+   **/
+  _roleChanged: function(old, newRole) {
+    if ( this._isValidRole(newRole) ) {
+      if ( old != newRole ) {
+        this.childNodes.array().filter(function(el) {
+          return el.isASQElement;
+        }).forEach(function(x) {
+          x.role = newRole;
+        });
+
+        // redo the $
+      }
+    } else {
+      this.role = old;
+    }
+  }
+};
+
+ASQ.asqElementBehavior = [elementTypeBehavior, roleBehavior];
+ASQ.asqQuestionElementBehavior = [elementTypeBehavior, questionTypeBehavior, roleBehavior];
